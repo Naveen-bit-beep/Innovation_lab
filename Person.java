9df7462c4712a0c78510547acbaa4413c1357caf
Naveen <24bcs026@kgcas.com>
2025-02-26 15:21:33 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,14 @@
+class person {
+String name;
+int age;
+public Person(String name.int age);
+this.name=name;
+this.age=age;
+ System.out.println("Welcome");
+ System.out.println("Name;"+name);
+ System.out.println("age;"+age);
+}
+public  static void main (String[] args){
+Person obj=new Person("Rakshan",19);
+}
+}
